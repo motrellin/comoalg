294f4f8de18f90b942c272fd6d24e8f4553bfd63
Max Ole Elliger <ole.elliger@fau.de>
2024-05-28 14:37:33 +0200

    bijective, injective, surjective

--- theories/Setoids.v
+++ theories/Setoids.v
@@ -16,7 +16,7 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *)
 
-From Coq Require Export RelationClasses Morphisms.
+From Coq Require Export Setoid RelationClasses Morphisms.
 
 Generalizable Variables X Y.
 
@@ -53,10 +53,83 @@ Class Setoid_Morph (X Y : Setoid) :=
     morph_combat :: Proper (carreq ==> carreq) morph
   }.
 
-(** A morphism is called _bijective_ if it has some inverse morphism. *)
+Section predicates.
 
-Definition bijective {X Y} (f : Setoid_Morph X Y) :=
-  {g : Setoid_Morph Y X |
-    (forall (y : @carr Y), morph (morph y) =s= y) /\
-    (forall (x : @carr X), morph (morph x) =s= x)
-  }.
+  Context `(f : @Setoid_Morph X Y).
+
+  Definition injective : Type :=
+    forall x y,
+      morph x =s= morph y ->
+      x =s= y.
+
+  Definition surjective : Type :=
+    forall y,
+      {x : carr | morph x =s= y}.
+
+  (** A morphism is called _bijective_ if it has some inverse morphism. *)
+
+  Definition bijective : Type :=
+    {g : Setoid_Morph Y X |
+      (forall (y : @carr Y), morph (morph y) =s= y) /\
+      (forall (x : @carr X), morph (morph x) =s= x)
+    }.
+
+  Lemma bijective_injective : 
+    bijective ->
+    injective.
+  Proof.
+    intros [g [H1 H2]] x y H3.
+    rewrite <- H2.
+    rewrite H3.
+    rewrite H2.
+    reflexivity.
+  Qed.
+
+  Lemma bijective_surjective :
+    bijective ->
+    surjective.
+  Proof.
+    intros [g [H1 H2]] y.
+    exists (morph y).
+    rewrite H1.
+    reflexivity.
+  Qed.
+
+  Lemma injective_surjective_bijective : 
+    injective ->
+    surjective ->
+    bijective.
+  Proof.
+    unfold injective, surjective, bijective.
+    intros H1 H2.
+    unshelve eexists.
+    {
+      unshelve econstructor.
+      -
+        intros y.
+        destruct (H2 y).
+        exact x.
+      -
+        simpl.
+        intros y1 y2 H3.
+        destruct (H2 y1) as [x1 H4], (H2 y2) as [x2 H5].
+        apply H1.
+        rewrite H4.
+        rewrite H5.
+        exact H3.
+    }
+    split.
+    -
+      intros y.
+      simpl.
+      destruct (H2 y) as [x H3].
+      exact H3.
+    -
+      intros x.
+      simpl.
+      destruct (H2 (morph x)) as [x' H3].
+      apply H1.
+      exact H3.
+  Qed.
+      
+End predicates.
